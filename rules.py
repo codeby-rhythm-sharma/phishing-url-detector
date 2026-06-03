d4b852ea5ef10ea46d756f2c9dd159d55f1fe2c0
Rhythm Sharma <rhythmm.sharma10@gmail.com>
2026-06-03 13:50:50 +0530

    Create rules.py

---
+++ rules.py
@@ -0,0 +1,27 @@
+SUSPICIOUS_KEYWORDS = [
+"login",
+"verify",
+"secure",
+"update",
+"banking",
+"signin",
+"account",
+"confirm",
+"password",
+]
+
+SHORTENERS = [
+"bit.ly",
+"tinyurl.com",
+"goo.gl",
+"t.co",
+"ow.ly",
+]
+
+SUSPICIOUS_TLDS = [
+".tk",
+".xyz",
+".top",
+".gq",
+".ml",
+]
